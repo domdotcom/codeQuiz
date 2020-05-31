db917f12f3dc2e0c6924b1e0dfd6982718de1bb1
Dominika <dominikaperkowska@gmail.com>
2020-05-31 19:16:02 -0400

    added quiz questions

--- scripts.js
+++ scripts.js
@@ -2,138 +2,178 @@
 const startQuizBtn = document.getElementById('quizStart')
 const introTxt = document.getElementById('quizChallenge')
 const timerElement = document.getElementById('timer')
-const questionElement = document.getElementById('question-container')
+const questionDiv = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerElement = document.getElementById('answerBtns')
+
+
+let questionsRandom, currentQuestion
 
 startQuizBtn.addEventListener('click', startQuiz)
 
 
-// functions needed
+// functions needed...
+// start the quiz qith Start quiz button
 function startQuiz(){
   // console.log('Started');
   startQuizBtn.classList.add('hide')
   introTxt.classList.add('hide')
   timerElement.classList.remove('hide')
-  questionElement.classList.remove('hide')
-
-
+  questionDiv.classList.remove('hide')
+  currentQuestion = 0
+  questionsRandom = questions.sort(() => Math.random() - 0.5)
+  nxtQuestion()
 }
 
 function nxtQuestion(){
+  resetState()
+  showQuestion(questionsRandom[currentQuestion])
 
+}
+function showQuestion(question){
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.className = 'btn-success'
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+
+    button.addEventListener('click', answerSelection)
+    answerElement.appendChild(button)
+  })
+}
+// reset everything in between questions
+function resetState(){
+  while (answerElement.firstChild) {
+    answerElement.removeChild(answerElement.firstChild)
+  }
 }
 
 function answerSelection(){
+  const selected = e.target
+  const correct = selected.data.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (questionsRandom.length > )
+}
 
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else{
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
 
-
 // quiz questions
-const myQuestions = 
-    [{
-      question: "How would you display 'Hello World' in an alert??",
-      answers: {
-        a: "msg('Hello World')",
-        b: "alert('Hello World');",
-        c: "alertBox('Hello World');",
-        d: "send(Hello World)"
-      },
-      correctAnswer: "b"
+const questions = [
+  {
+    question: "How would you display 'Hello World' in an alert?",
+      answers: [
+        {text: "msg('Hello World')", correct: false},
+        {text: "alert('Hello World');", correct: true},
+        {text: "alertBox('Hello World');", correct: false},
+        {text: "send(Hello World)", correct: false}
+      ]
+  },
+  {
+    question: "Commonly used data types DO NOT include: ",
+      answers: [
+        {text: "booleans", correct: false},
+        {text: "strings", correct: false},
+        {text: "alerts", correct: true},
+        {text: "numbers", correct: false}
+      ]
     },
-    {
-      question: "Commonly used data types DO NOT include: ",
-      answers: {
-        a: "booleans",
-        b: "alerts",
-        c: "numbers",
-        d: "strings"
-      },
-      correctAnswer: "b"
+  {
+    question: "Arrays in Javascript can be used to store the following: ",
+      answers: [
+        {text: "other arrays", correct: false},
+        {text: "numbers and strings", correct: false},
+        {text: "booleans", correct: false},
+        {text: "all of the above", correct: true}
+      ]
     },
-    {
-      question: "Arrays in Javascript can be used to store the following: ",
-      answers: {
-        a: "other arrays",
-        b: "booleans",
-        c: "numbers and strings",
-        d: "all of the above"
-      },
-      correctAnswer: "d"
+  {
+    question: "What will this output? var a = [1, 2, 3]; console.log(a[6]);",
+      answers: [
+        {text: "undefined", correct: true},
+        {text: "6", correct: false},
+        {text: "NaN", correct: false},
+        {text: "syntax error", correct: false}
+      ]
     },
-    {
-        question: "What will this output? var a = [1, 2, 3]; console.log(a[6]);",
-        answers: {
-            a: "undefined",
-            b: "NaN",
-            c: "void",
-            d: "6"
+  {
+    question: "What is the condition in an if/else statment enclosed in?",
+      answers: [
+        {text: "square brackets", correct: false},
+        {text: "single quotes", correct: false},
+        {text: "nothing", correct: false},
+        {text: "parenthesis", correct: true}
+      ]
     },
-        correctAnswer: "a"
+  {
+    question: "What does the following selector: $('div') select?",
+      answers: [
+        {text: "first div element", correct: false},
+        {text: "all div elements", correct: true},
+        {text: "last div element", correct: false},
+        {text: "not a selector", correct: false}
+      ]
     },
-    {
-        question: "What is the condition in an if/else statment enclosed in?",
-        answers: {
-          a: "quotes",
-          b: "nothing",
-          c: "parenthesis",
-          d: "square brackets"
-        },
-        correctAnswer: "c"
+  {
+    question: "Which is a useful tool during development and debugging for printing content to the debugger?",
+      answers: [
+        {text: "terminal.bash", correct: false},
+        {text: "for loops", correct: false},
+        {text: "Java", correct: false},
+        {text: "console.log", correct: true}
+      ]
     },
-    {
-        question: "What does the following selector: $('div') select?",
-        answers: {
-          a: "the first div element",
-          b: "the last div element",
-          c: "all div elements'",
-          d: "its not a selector"
-        },
-        correctAnswer: "c"
+  {
+    question: "Which for loop runs from 0 to 9?",
+      answers: [
+        {text: "for (i=0: i<10; i++)", correct: false},
+        {text: "for (i<10; i = 0; i++)", correct: false},
+        {text: "for (i = 0; i < 10; i++)", correct: true},
+        {text: "for (i =0, i < 10, i+)", correct: false}
+      ]
     },
-    {
-        question: "Which is a useful tool during development and debugging for printing content to the debugger?",
-        answers: {
-          a: "Javascript",
-          b: "for loops",
-          c: "terminal/bash",
-          d: "console.log"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "Which for loop runs from 0 to 9?",
-        answers: {
-          a: "for (i=0: i<10; i++)",
-          b: "for (i<10; i = 0; i++)",
-          c: "for (i = 0; i < 10; i++)",
-          d: "for (i =0, i < 10, i+) "
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "What is the proper syntax for calling a function?",
-        answers: {
-          a: "myFunction();",
-          b: "callMyFunction;",
-          c: "function myFunction();",
-          d: "myFunction;"
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "Use the correct Array method to remove the last item of this array; fruits : ['mango', 'kiwi', 'apple', 'guava'].",
-        answers: {
-          a: "remove(last);",
-          b: "fruits[3-1];",
-          c: "fruits.pop();",
-          d: "fruits(pop);"
-        },
-        correctAnswer: "c"
+  {
+  question: "What is the proper syntax for calling a function?",
+    answers: [
+      {text: "myFunction();", correct: true},
+      {text: "callMyFunction;", correct: false},
+      {text: "function myFunction();", correct: false},
+      {text: "myFunction;", correct: false}
+    ]
+  },
+  {
+    question: "Use the correct Array method to remove the last item of this array; fruits : ['mango', 'kiwi', 'apple', 'guava'].",
+      answers: [
+        {text: "remove(last);", correct: false},
+        {text: "fruits[3-1];", correct: false},
+        {text: "fruits.pop();", correct: true},
+        {text: "fruits(pop)", correct: false}
+      ]
     }
-  ];
+  ]
 
 
 
+  //  
 
 
 
